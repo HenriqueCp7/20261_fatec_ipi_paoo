b986b98406c2a52e3ce185ffcb8be82d773e56ce
HenriquePereira <henriquecpereira286@gmail.com>
2026-05-26 11:51:34 -0300

    feat(p2): implementa endpoint de consulta de informacoes de moderacao

--- microsservicos/moderacao/index.js
+++ microsservicos/moderacao/index.js
@@ -4,14 +4,37 @@ const app = express()
 app.use(express.json())
 
 const blacklist = ['avenida', 'gato', 'pastel', 'uva', 'fruta']
+const informacoes = {
+  qntDetectadas: {},
+  lembretesBloqueados: 0,
+  palavrasEmLembretes: 0,
+  observacoesBloqueadas: 0,
+  palavrasEmObservacoes: 0
+}
 
 function palavraProibida(texto) {
   return blacklist.some(palavra => texto.includes(palavra))
 }
 
+function registrarInformacoes(texto, tipo) {
+  const encontradas = blacklist.filter(palavra => texto.includes(palavra))
+  encontradas.forEach(palavra => {
+    informacoes.qntDetectadas[palavra] = (informacoes.qntDetectadas[palavra] || 0) + 1
+  })
+  const quantidade = encontradas.reduce((x, vet) => x + 1, 0)
+  if (tipo === 'lembrete') {
+    informacoes.lembretesBloqueados++
+    informacoes.palavrasEmLembretes += quantidade
+  } else {
+    informacoes.observacoesBloqueadas++
+    informacoes.palavrasEmObservacoes += quantidade
+  }
+}
+
 app.post('/lembretes', async (req, res) => {
   const { texto } = req.body
   if (palavraProibida(texto)) {
+    registrarInformacoes(texto, 'lembrete')
     return res.status(400).end()
   }
   await axios.post('http://localhost:10000/eventos', {
@@ -24,6 +47,7 @@ app.post('/lembretes', async (req, res) => {
 app.post('/lembretes/:id/observacoes', async (req, res) => {
   const { texto } = req.body
   if (palavraProibida(texto)) {
+    registrarInformacoes(texto, 'lembrete')
     return res.status(400).end()
   }
   await axios.post('http://localhost:10000/eventos', {
@@ -31,6 +55,20 @@ app.post('/lembretes/:id/observacoes', async (req, res) => {
     dados: { texto, lembreteId: req.params.id }
   })
   res.status(201).end()
+})
+
+app.get('/informacoes/:palavra', (req, res) => {
+  const palavra = req.params.palavra
+  res.json({
+    [`Informacoes sobre a palavra: ${palavra}`]: 
+    {
+      qntDeVezesDetectada: informacoes.qntDetectadas[palavra] || 0,
+      mediaDeVezesEmLembretes: informacoes.lembretesBloqueados === 0 ? 0
+        : informacoes.palavrasEmLembretes / informacoes.lembretesBloqueados,
+      mediaDeVezesEmObservacao: informacoes.observacoesBloqueadas === 0 ? 0
+        : informacoes.palavrasEmObservacoes / informacoes.observacoesBloqueadas
+    }
+  })
 })
 
 const port = 9000
